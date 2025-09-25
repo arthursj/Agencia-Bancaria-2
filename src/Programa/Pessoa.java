ff2bc4d87c5dfaf7d14d54852356ab83f31c36e0
Arthur Silva <arthurcom898@gmail.com>
2025-09-25 18:31:06 -0300

    Implementing formatting method

--- src/Programa/Pessoa.java
+++ src/Programa/Pessoa.java
@@ -32,4 +32,10 @@ public class Pessoa {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String toString() {
+        return "\nNome: " + this.getNome() +
+                "\nCPF: " + this.getCPF() +
+                "\nEmail: " + this.getEmail();
+    }
 }
