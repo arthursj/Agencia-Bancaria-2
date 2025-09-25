8c3c07f8c2ccaf9314489412961bf45f6fd7e971
Arthur Silva <arthurcom898@gmail.com>
2025-09-25 19:08:09 -0300

    Add account creation counter in ascending order

--- src/Programa/Conta.java
+++ src/Programa/Conta.java
@@ -1,6 +1,9 @@
 package Programa;
 
 public class Conta {
+
+    private static int contadorDeContas = 1;
+
     private int numeroConta;
     private Pessoa pessoa;
     private Double saldo = 0.0;
