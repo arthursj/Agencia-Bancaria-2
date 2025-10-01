24e4cdc076063e1ad9bcfead03aa5477ed41659d
Arthur Silva <arthurcom898@gmail.com>
2025-10-01 10:37:52 -0300

    Defining depositar, sacar and transferir methods

--- src/Programa/Conta.java
+++ src/Programa/Conta.java
@@ -48,4 +48,33 @@ public class Conta {
                "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
                "\n";
     }
+
+    public void depositar(Double valor) {
+        if(valor > 0) {
+            setSaldo(getSaldo() + valor);
+            System.out.println("Seu depósito foi realizado com sucesso!");
+        } else {
+            System.out.println("Não foi possível realizar o depósito!");
+        }
+    }
+
+    public void sacar(Double valor) {
+        if (valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            System.out.println("Não foi possível realizar o saque!");
+        }
+    }
+
+    public void transferir(Conta contaParaDeposito, Double valor) {
+        if (valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            
+            contaParaDeposito.saldo =  contaParaDeposito.getSaldo() + valor;
+            System.out.println("Transferência realizada com sucesso!");
+        } else {
+            System.out.println("Não foi possível realizar a transferência!");
+        }
+    }
 }
