9b32c8991caa19bf11940f367599b4a16bd58d74
Arthur Silva <arthurcom898@gmail.com>
2025-10-03 10:35:06 -0300

    Creating a list of accounts of type Conta called contasBancarias

--- src/Programa/AgenciaBancaria.java
+++ src/Programa/AgenciaBancaria.java
@@ -1,5 +1,14 @@
 package Programa;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AgenciaBancaria {
     
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Conta> contasBancarias;
+
+    public static void main(String[] args) {
+        contasBancarias = new ArrayList<Conta>();
+    }
 }
