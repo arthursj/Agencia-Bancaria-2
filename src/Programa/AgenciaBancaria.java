f3707519cb78c400fb6ed6977407ced34fc7bfe3
Arthur Silva <arthurcom898@gmail.com>
2025-10-03 10:44:16 -0300

    Creating a user operations menu with the operacoes method

--- src/Programa/AgenciaBancaria.java
+++ src/Programa/AgenciaBancaria.java
@@ -10,5 +10,22 @@ public class AgenciaBancaria {
 
     public static void main(String[] args) {
         contasBancarias = new ArrayList<Conta>();
+        operacoes();
+    }
+
+    public static void operacoes() {
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("--------------- Bem-vindo a nossa Agência ----------------");
+        System.out.println("--------------------------------------------------");
+        System.out.println("***** Selecione uma operação que deseja realizar *****");
+        System.out.println("--------------------------------------------------");
+        System.out.println("|   1 - Criar conta     |");
+        System.out.println("|   2 - Depositar       |");
+        System.out.println("|   3 - Sacar           |");
+        System.out.println("|   4 - Transferir      |");
+        System.out.println("|   5 - Listar contas   |");
+        System.out.println("|   6 - Sair            |");
+
     }
 }
